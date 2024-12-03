c5c04df2df8468b059beff17206d9b8039851711
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-12-03 12:17:18 -0600

    Messing with data types

--- The basics/start.c
+++ The basics/start.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
@@ -6,20 +7,36 @@ int main()
 
     int age = 22;
     float balls = 2.45;
-
-    // Can only store a single character
     char p = 'p';
-
-    // Storing multiple characters, a string is technically an array of chars
-    char name[] = "Gabriel";
+    char name[] = "Goob";
 
     //---Print/console log:---
 
     // \ followed by any character is an escape sequence, specifying actions within a string or text
-    printf("Goob is a \t nerd.\n");
-    printf("Goob is %d years old.\n", age);
-    printf("I'm \'watching\' you right now.");
+    // Using % in a string will allow you to insert variables into the print statement
+    printf("%s is a \t nerd and hates the letter %c.\n", name, p);
+    printf("%s is %d years old.\n", name, age);
+    printf("%s is \'watching\' you right now, you have %0.2f seconds before you explode.", name, balls);
 
-    // Will return 0 if there are no errors, otherwise will return 1
-    return 0;
+    // --Datatypes:--
+
+    int day = 10; // Single character %c
+
+    char letters[] = "abcd"; // Array of characters
+
+    float secondsBeforeDetonation = 14.790520483923; // 4 bytes, 32 bits of precision (6-7 digits) %f
+
+    double d = 3.110375524210245; // 8 bytes, 64 bits of precision (15-16 digits) %lf
+
+    bool isAwesome = true; // 1 byte, true/false, %d
+
+    char f1 = 100; // 1 byte, -128 through 127 %d (WILL PRINT NUM) or %c (USES ASCII TABLE CONVERSION)
+
+    unsigned char f2 = 255; // 1 byte, 0 through 255 %d (WILL PRINT NUM) or %c (USES ASCII TABLE CONVERSION)
+
+    short int benis1 = 32767; // 2 bytes (-32,768 to 32,767) %d
+
+    unsigned short int benis2 = 65535; // 2 bytes (0 to 65,535) %d
+
+    return 0; // Will return 0 if there are no errors, otherwise will return 1
 }
