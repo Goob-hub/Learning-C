5cf335c50f14af5bfe3152e7964fd1b5ff9b903c
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-12-05 15:19:10 -0600

    area of circle is now also calculated
    ayup, just as the title of the commit says

--- The basics/math/circumference.c
+++ The basics/math/circumference.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
     const double PI = 3.14159;
     double circumference;
     double radius;
+    double area;
 
     printf("\n Enter the radius of a circle doofus: ");
 
@@ -12,7 +14,9 @@ int main()
 
     circumference = 2 * PI * radius;
 
-    printf("\n The circumference of your circle is %lf", circumference);
+    area = PI * pow(radius, 2.0);
+
+    printf("\n The circumference of your circle is %lf. \n The area of your circle is %lf.", circumference, area);
 
     return 0;
 }
