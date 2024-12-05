bf68d05a796910fa49a606cfa10a2741900124d2
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-12-05 15:01:56 -0600

    working with user input and math functions
    yep

---
+++ The basics/math/math.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double A = sqrt(9);
+    double B = pow(2, 4);
+    int C = round(3.14);
+    int D = ceil(3.14);
+    int E = floor(3.99);
+    double F = fabs(-100); // Turn negative number positive basically
+    double G = log(3);
+    double H = cos(45);
+    double I = tan(45);
+    double J = sin(45);
+
+    printf("\n bollocks, your life will end in %0.1lf seconds", A);
+
+    return 0;
+}
--- The basics/user_input/user_input.c
+++ The basics/user_input/user_input.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
+    char name[25]; // 25 byte character limit
     int age;
 
-    printf("\nHow old are you Mr.Freakbob?");
+    printf("\nWhat is your full name nerd?");
+    // scanf("%s", &name);
+    fgets(name, 25, stdin); // Will include new line space at end of string, but reads all spaces in string
+
+    name[strlen(name) - 1] = '\0'; // gets rid of new line character at end of string
+
+    printf("\nHow old are you %s?", &name);
     scanf("%d", &age); // Will look for user input in form of integer, defaults to 0
 
-    printf("Says here that you're %d years old Freakbob.", age);
+    printf("\nSays here that you're %d years old %s.", age, name);
 
     return 0;
 }
