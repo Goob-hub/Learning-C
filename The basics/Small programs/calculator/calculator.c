fc1fff3f71df94412242181704f8bdd30ae1624e
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-12-10 14:16:22 -0600

    Made simple calculator
    as the title says

---
+++ The basics/Small programs/calculator/calculator.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main()
+{
+    char operator;
+    int value;
+    int num1;
+    int num2;
+
+    printf("\nInput the first number for your operation: ");
+    scanf("%d", &num1);
+
+    printf("\nEnter the second number for your operation: ");
+    scanf("%d", &num2);
+
+    printf("\nEnter the type of operation you want to perform: ");
+    scanf(" %c", &operator);
+
+    if (operator== '+')
+    {
+        value = num1 + num2;
+    }
+    else if (operator== '-')
+    {
+        value = num1 - num2;
+    }
+    else if (operator== '*')
+    {
+        value = num1 * num2;
+    }
+    else if (operator== '/')
+    {
+        value = num1 / num2;
+    }
+    else
+    {
+        printf("\nYou did not enter a valid operator!");
+    }
+
+    printf("\nThe value of your equation is: %d", value);
+
+    return 0;
+}
