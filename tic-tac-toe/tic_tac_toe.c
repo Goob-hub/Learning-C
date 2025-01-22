430786b604c1549fd977a7b00fe73d2f2663e2f5
Goob-tech <solizg02@gmail.com>
2025-01-22 16:44:39 -0600

    User can now input onto the board
    yummers

--- tic-tac-toe/tic_tac_toe.c
+++ tic-tac-toe/tic_tac_toe.c
@@ -4,65 +4,36 @@
 #include <time.h>
 #include <stdbool.h>
 
+void updateBoard(bool isPlayersTurn, int coordinates[2], char board[3][3]);
+void playerInput(int coordinates[2]);
 void displayBoard(char board[3][3]);
-void playerInput(void);
-void insertBoardData(int col, int row);
 
 int main() {
     
     //Create tic tac toe board as a matrix of char
     //Make and display board in console
     //Prompt user for row and column for insertion of their char
+    
     //Only allow characters to be entered in empty squares
     //Create a function that checks for a winner after every input
     //Create tic tac toe bot for player to play against
 
     char board[3][3] = {{' ', ' ', ' '},{' ', ' ', ' '},{' ', ' ', ' '}};
+    int coordinates[2];
     bool winner = false;
 
     do
     {
         displayBoard(board);
-        playerInput();
+        playerInput(coordinates);
+        updateBoard(true, coordinates, board);
+        //Bot logic function
+        //update board for bot
+        //Check for winner
         winner = true;
     } while (!winner);
     
     return 0;
-}
-
-void playerInput() {
-    int col = 0;
-    int row = 0;
-
-    do
-    {
-        printf("Enter column (#1-3): ");
-
-        if (scanf("%d", &col) != 1) {
-            // Input was not an integer
-            printf("Invalid input. Please enter a number.\n");
-            // Clear the input buffer
-            while (getchar() != '\n'); 
-        } else {
-            // Input was successfully read
-            printf("You entered: %d\n", col);
-        }
-    } while (col == 0);
-
-    do
-    {
-        printf("\nEnter row (#1-3): ");
-        
-        if (scanf("%d", &row) != 1) {
-            // Input was not an integer
-            printf("Invalid input. Please enter a number.\n");
-            // Clear the input buffer
-            while (getchar() != '\n'); 
-        } else {
-            // Input was successfully read
-            printf("You entered: %d\n", row);
-        }
-    } while (row == 0);
 }
 
 void displayBoard(char board[3][3]) {
@@ -72,5 +43,41 @@ void displayBoard(char board[3][3]) {
         if(i < 2) {
             printf("---|---|---\n");
         }
+    }
+}
+
+void playerInput(int coordinates[2]) {
+    do
+    {
+        printf("Enter column (#1-3): ");
+
+        if (scanf("%d", &coordinates[0]) != 1) {
+            printf("Invalid input. Please enter a number.\n");
+            while (getchar() != '\n'); // Clear the input buffer 
+        } else if(coordinates[0] > 3 || coordinates[0] < 1) {
+            coordinates[0] = 0;
+            printf("Please enter a valid number (#1-3).\n");
+        }
+    } while (coordinates[0] == 0);
+
+    do
+    {
+        printf("\nEnter row (#1-3): ");
+        
+        if (scanf("%d", &coordinates[1]) != 1) {
+            printf("Invalid input. Please enter a number (#1-3).\n");
+            while (getchar() != '\n'); // Clear the input buffer 
+        } else if(coordinates[1] > 3 || coordinates[1] < 1) {
+            coordinates[1] = 0;
+            printf("Please enter a valid number (#1-3).\n");
+        }
+    } while (coordinates[1] == 0);
+}
+
+void updateBoard(bool isPlayersTurn, int coordinates[2], char board[3][3]) {
+    if(isPlayersTurn) {
+        board[coordinates[0] - 1][coordinates[1] - 1] = 'X';
+    } else {
+        board[coordinates[0] - 1][coordinates[1] - 1] = 'O';
     }
 }
