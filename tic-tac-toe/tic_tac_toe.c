4bb04d2c99430009cf7776377f684015fe086f6b
Goob-tech <solizg02@gmail.com>
2025-01-26 15:30:13 -0600

    Finished tic tac toe
    yummers

--- tic-tac-toe/tic_tac_toe.c
+++ tic-tac-toe/tic_tac_toe.c
@@ -4,34 +4,76 @@
 #include <time.h>
 #include <stdbool.h>
 
-void updateBoard(bool isPlayersTurn, int coordinates[2], char board[3][3]);
+bool updateBoard(bool isPlayersTurn, int coordinates[2], char board[3][3]);
+int checkForWinner(char board[3][3]);
 void playerInput(int coordinates[2]);
 void displayBoard(char board[3][3]);
+int checkDiagonal(char board[3][3]);
+int checkColumns(char board[3][3]);
+int checkRows(char board[3][3]);
 
 int main() {
-    
-    //Create tic tac toe board as a matrix of char
-    //Make and display board in console
-    //Prompt user for row and column for insertion of their char
-    
-    //Only allow characters to be entered in empty squares
-    //Create a function that checks for a winner after every input
-    //Create tic tac toe bot for player to play against
-
     char board[3][3] = {{' ', ' ', ' '},{' ', ' ', ' '},{' ', ' ', ' '}};
+    bool validMove = true;
+    bool gameOver = false;
     int coordinates[2];
-    bool winner = false;
+    int winner = -1;
 
     do
     {
-        displayBoard(board);
-        playerInput(coordinates);
-        updateBoard(true, coordinates, board);
-        //Bot logic function
-        //update board for bot
-        //Check for winner
-        winner = true;
-    } while (!winner);
+        printf("--------------------------");
+        printf("\nPlayer 1 it's your turn!\n");
+        printf("--------------------------\n");
+
+        do
+        {
+            if(!validMove) {
+                printf("-----------------------------------------------------------");
+                printf("\nYour move was not in an empty square, please try again!\n");
+                printf("-----------------------------------------------------------\n");
+            }
+            displayBoard(board);
+            playerInput(coordinates);
+            validMove = updateBoard(true, coordinates, board);
+
+        } while (!validMove);
+
+        winner = checkForWinner(board);
+
+        if(winner != -1) {
+            break;
+        }
+
+        coordinates[0] = 0;
+        coordinates[1] = 0;
+
+        printf("--------------------------");
+        printf("\nPlayer 2 it's your turn!\n");
+        printf("--------------------------\n");
+
+         do
+        {
+            if(!validMove) {
+                printf("-----------------------------------------------------------");
+                printf("\nYour move was not in an empty square, please try again!\n");
+                printf("-----------------------------------------------------------\n");
+            }
+            displayBoard(board);
+            playerInput(coordinates);
+            validMove = updateBoard(false, coordinates, board);
+
+        } while (!validMove);
+
+        winner = checkForWinner(board);
+    } while (winner == -1);
+
+    if(winner == -1) {
+        printf("\nNOBODY WON WOMP WOMP\n");
+    } else if(winner == 0) {
+        printf("\nGOOD JOB PLAYER ONE YOU DID A THING\n");
+    } else {
+        printf("\nGOOD JOB PLAYER 2 YOU DID A THING\n");
+    }
     
     return 0;
 }
@@ -74,10 +116,98 @@ void playerInput(int coordinates[2]) {
     } while (coordinates[1] == 0);
 }
 
-void updateBoard(bool isPlayersTurn, int coordinates[2], char board[3][3]) {
-    if(isPlayersTurn) {
+bool updateBoard(bool isPlayerOneTurn, int coordinates[2], char board[3][3]) {
+
+    if(board[coordinates[0] - 1][coordinates[1] - 1] != ' ') {
+        return false;
+    }
+
+    if(isPlayerOneTurn) {
         board[coordinates[0] - 1][coordinates[1] - 1] = 'X';
     } else {
         board[coordinates[0] - 1][coordinates[1] - 1] = 'O';
     }
+
+    return true;
+}
+
+int checkForWinner(char board[3][3]) {
+    int winner = -1;
+
+    winner = checkRows(board);
+    if(winner != -1) {return winner;}
+
+    winner = checkColumns(board);
+    if(winner != -1) {return winner;}
+
+    winner = checkDiagonal(board);
+    if(winner != -1) {return winner;}
+
+    return winner;
+}
+
+int checkDiagonal(char board[3][3]) {
+    if(board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') {
+        return 0;
+    } else if(board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') {
+        return 1;
+    } else if(board[2][0] == 'X' && board[1][1] == 'X' && board[0][2] == 'X') {
+        return 0;
+    } else if(board[2][0] == 'O' && board[1][1] == 'O' && board[0][2] == 'O') {
+        return 1;
+    }
+
+    return -1;
+}
+
+int checkColumns(char board[3][3]) {
+    for (int i = 0; i < 3; i++)
+    {
+        int xCount = 0;
+        int oCount = 0;
+
+        for(int j = 0; j < 3; j++) {
+            if(board[j][i] == 'X') {
+                xCount++;
+            } else if(board[j][i] == 'O') {
+                oCount++;
+            } 
+        }
+
+        if(xCount == 3) {
+            return 0;
+        }
+
+        if(oCount == 3) {
+            return 1;
+        }
+    }
+
+    return -1;
+}
+
+int checkRows(char board[3][3]) {
+    for (int i = 0; i < 3; i++)
+    {
+        int xCount = 0;
+        int oCount = 0;
+
+        for(int j = 0; j < 3; j++) {
+            if(board[i][j] == 'X') {
+                xCount++;
+            } else if(board[i][j] == 'O') {
+                oCount++;
+            } 
+        }
+
+        if(xCount == 3) {
+            return 0;
+        }
+
+        if(oCount == 3) {
+            return 1;
+        }
+    }
+
+    return -1;
 }
